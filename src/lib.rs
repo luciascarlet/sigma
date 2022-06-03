5f2476a8d16db44651d8c758c464b37396e093fd
Lucia Scarlet <lucia@shadowblood.red>
2022-06-03 13:13:41 +0200

    ffs

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,10 @@ pub mod graph;
 pub mod operators;
 pub mod synthesis;
 
-#cfg[test]
-
+#[cfg(test)]
 fn it_doesnt_work() {
 	// im not even going to bother with an actual unit test i just want to see if the garbage my code generates even makes any sense
 
 	// create sine bank to work with
-	let sine_bank = synthesis::sine_bank::SineBank
+	let sine_bank = synthesis::SineBank::new(2048, 80.0, 44100.0);
 }
