ae3ee1262a57954b1a5b2502fe21aaef5e1f6483
Lucia Scarlet <lucia@shadowblood.red>
2022-06-03 12:19:34 +0200

    get rid of commented-out nonsense

--- src/operators/filter_op.rs
+++ src/operators/filter_op.rs
@@ -1,32 +1,5 @@
 use crate::synthesis::partial::Partial;
 
-// // base trait for all filter ops
-// pub trait FilterOp {
-// }
-
-// // these are free to be implemented simultaneously on a single filter op if desired
-// pub trait PhaseOp: FilterOp {
-// 	fn process_phases(&self, phases: Vec<f64>, fundamental: f64) -> Vec<f64>;
-// }
-
-// pub trait RatioOp: FilterOp {
-// 	fn process_ratios(&self, ratios: Vec<f64>, fundamental: f64) -> Vec<f64>;
-// }
-
-// pub trait AmpOp: FilterOp {
-// 	fn process_amps(&self, amps: Vec<f64>, fundamental: f64) -> Vec<f64>;
-// }
-
-// pub trait StereoOp: FilterOp {
-// 	fn process_pans(&self, pans: Vec<f64>, fundamental: f64) -> Vec<f64>;
-// }
-
-// // this one is special because it operates on full partial entries
-// pub trait PartialOp: FilterOp {
-// 	fn process_partials(&self, partials: Vec<Partial>) -> Vec<Partial>;
-// }
-
-
 // you know what, I don't think I need all of this shit - let's just make everything PartialOp
 pub trait FilterOp {
 	fn transform(&self, partial: Partial, idx: usize, fundamental: f64) -> Partial;
