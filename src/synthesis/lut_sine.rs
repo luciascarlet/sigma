e923d8c8e559dbf8fb59694e37558b900cb80376
Lucia Scarlet <lucia@shadowblood.red>
2022-06-06 02:34:35 +0200

    unfuck sine LUT for once and for all (it actually works)

--- src/synthesis/lut_sine.rs
+++ src/synthesis/lut_sine.rs
@@ -1,3 +1,4 @@
+use crate::maths::lerp;
 use core::f64::consts::PI;
 
 pub struct LUTSine {
@@ -7,11 +8,12 @@ pub struct LUTSine {
 
 impl LUTSine {
 	pub fn new(size: usize) -> LUTSine {
-		let mut lut = vec![0.0; size + 1];
+		let mut lut = vec![0.0; size + 2];
 		for i in 0..size {
-			lut[i] = (i as f64 / size as f64 * PI / 2.0).sin();
+			lut[i] = (i as f64 / size as f64 * PI * 0.5).sin();
 		}
-		lut[size + 1] = 1.0; // for interpolation
+		lut[size] = 1.0; // for interpolation
+		lut[size + 1] = 1.0; // dumbass fucking hack that i can't be arsed to actually fix
 		LUTSine {
 			lut: lut,
 			size: size,
@@ -21,23 +23,38 @@ impl LUTSine {
 	// return sine wave sample at a given phase between 0.0 and 1.0
 	pub fn sample(&self, phase: f64) -> f64 {
 		let phase = phase % 1.0;
-		let lerp = (phase * self.size as f64).fract();
 		match phase {
 			x if x >= 0.0 && x <= 0.25 => {
-				let peek = phase * self.size as f64;
-				self.lut[peek as usize] * (1.0 - lerp) + self.lut[peek as usize + 1] * lerp
+				let peek = phase * self.size as f64 * 4.0;
+				lerp(
+					self.lut[peek as usize],
+					self.lut[(peek + 1.0) as usize],
+					peek.fract(),
+				)
 			}
 			x if x > 0.25 && x <= 0.5 => {
-				let peek = (-phase + 0.5) * self.size as f64;
-				self.lut[peek as usize] * (1.0 - lerp) + self.lut[peek as usize + 1] * lerp
+				let peek = (-phase + 0.5) * self.size as f64 * 4.0;
+				lerp(
+					self.lut[peek as usize],
+					self.lut[(peek + 1.0) as usize],
+					peek.fract(),
+				)
 			}
 			x if x > 0.5 && x <= 0.75 => {
-				let peek = (phase - 0.5) * self.size as f64;
-				-self.lut[peek as usize] * (1.0 - lerp) + self.lut[peek as usize + 1] * lerp
+				let peek = (phase - 0.5) * self.size as f64 * 4.0;
+				-lerp(
+					self.lut[peek as usize],
+					self.lut[(peek + 1.0) as usize],
+					peek.fract(),
+				)
 			}
 			x if x > 0.75 && x <= 1.0 => {
-				let peek = (-phase + 1.0) * self.size as f64;
-				-self.lut[peek as usize] * (1.0 - lerp) + self.lut[peek as usize + 1] * lerp
+				let peek = (-phase + 1.0) * self.size as f64 * 4.0;
+				-lerp(
+					self.lut[peek as usize],
+					self.lut[(peek + 1.0) as usize],
+					peek.fract(),
+				)
 			}
 			_ => panic!("phase outside of 0-1; what the fuck?"),
 		}
